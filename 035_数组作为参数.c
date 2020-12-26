1266e20699a794fd73e50e1969afdb2699ae33b5
axin <hoojiaxin@163.com>
2020-12-26 17:28:14 +0800

    数组作为参数的问题

---
+++ 035_数组作为参数.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void BubbSort(int arr[], int len) {
+	
+	for (int i = 0; i < len - 1; i++) {
+		int flag = 1;
+		for (int j = 0; j < len - 1 - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+				flag = 0;
+			}
+		}
+		if (flag) {
+			break;
+		}
+	}
+
+}
+int main() {
+
+	int arr[] = { 3,2,5,8,7,1,11,12,10 };
+
+	int len = sizeof(arr) / sizeof(arr[0]);
+
+	//ц╟ещеепР
+	BubbSort(arr, len);
+	for (int i = 0; i < len; i++) {
+		printf("%d ", arr[i]);
+	}
+
+	return 0;
+}
